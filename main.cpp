8bd0238207de55e751fc9e40bd05f6622f6445f6
Andrew Obukhov <andrewrcr@mail.ru>
2021-10-04 16:21:34 +0300

    Duration logger added

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 
 #include "map.h"
 #include "finder_bfs.h"
+#include "profile.h"
 
 int main() {
     map m("../map.txt");
     m.printMap();
     std::cout << "--------------------" << std::endl;
 
-    std::deque<Point> route = findRoute(m);
+    std::deque<Point> route;
+    {
+        LOG_DURATION("Base algorithm time");
+        route = findRoute(m);
+    }
     m.markRoute(route);
     m.printMap();
     return 0;
